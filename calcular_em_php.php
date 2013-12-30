3f88e8dbf3dc70f62e886c28ef17ad0c2bf88760
João Eduardo Dias <diasje@gmail.com>
2013-12-30 12:36:33 +0000

    Create calcular_em_php.php

---
+++ calcular_em_php.php
@@ -0,0 +1,58 @@
+<?php echo "Aqui está uma forma simplificada de calcular usando código PHP.<br><br>"; ?>
+<?php
+echo (">> Multiplicar 2 x 3<br>");
+$num1 = ("2");
+$num2 =("3");
+$result = $num1*$num2;
+echo ("<b>Resultado = </b> $result");
+echo ("<br>");
+echo ("<br>");
+?>
+<?php echo "<b>Como está estruturado o código?</b><br>"; ?>
+<i>< ?php<br>
+$num1 = ("2");<br>
+$num2 =("3");<br>
+$result = $num1*$num2;<br>
+echo ("Resultado =  $result");<br>
+	? ></i>
+<br><br>
+
+
+
+
+<?php
+echo (">> Somar 2 + 3<br>");
+$num1 = ("2");
+$num2 =("3");
+$result = $num1+$num2;
+echo ("<b>Resultado = </b>$result");
+echo ("<br>");
+echo ("<br>");
+?>
+<?php echo "<b>Como está estruturado o código?</b><br>"; ?>
+<i>< ?php<br>
+$num1 = ("2");<br>
+$num2 =("3");<br>
+$result = $num1+$num2;<br>
+	echo ("Resultado = $result");<br>
+? ></i>
+
+
+<?php
+echo ("<br>");
+echo ("<br>");
+echo (">> Dividir 6 : 2<br>");
+$num1 = ("6");
+$num2 =("2");
+$result = $num1/$num2;
+echo ("<b>Resultado = </b> $result");
+echo ("<br>");
+echo ("<br>");
+?>
+<?php echo "<b>Como está estruturado o código?</b><br>"; ?>
+<i>< ?php<br>
+$num1 = ("6");<br>
+$num2 =("2");<br>
+$result = $num1/$num2;<br>
+echo ("Resultado =  $result");<br>
+	? ></i>
