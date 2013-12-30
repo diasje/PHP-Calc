84abb8824032b670f14e132edb7392d40a3be765
João Eduardo Dias <diasje@gmail.com>
2013-12-30 13:05:07 +0000

    Update calcular_em_php.php

--- calcular_em_php.php
+++ calcular_em_php.php
@@ -1,58 +1,38 @@
-<?php echo "Aqui está uma forma simplificada de calcular usando código PHP.<br><br>"; ?>
+
 <?php
+//mostramos o que pretendemos fazer
 echo (">> Multiplicar 2 x 3<br>");
+// definimos as variáveis
 $num1 = ("2");
 $num2 =("3");
+// Definimos a variável da conta de somar usando o +
 $result = $num1*$num2;
+//Mostra o resultado
 echo ("<b>Resultado = </b> $result");
-echo ("<br>");
-echo ("<br>");
 ?>
-<?php echo "<b>Como está estruturado o código?</b><br>"; ?>
-<i>< ?php<br>
-$num1 = ("2");<br>
-$num2 =("3");<br>
-$result = $num1*$num2;<br>
-echo ("Resultado =  $result");<br>
-	? ></i>
-<br><br>
-
-
-
 
 <?php
+//mostramos o que pretendemos fazer
 echo (">> Somar 2 + 3<br>");
+// definimos as variáveis
 $num1 = ("2");
 $num2 =("3");
+// Definimos a variável da conta de somar usando o +
 $result = $num1+$num2;
+//Mostra o resultado
 echo ("<b>Resultado = </b>$result");
-echo ("<br>");
-echo ("<br>");
 ?>
-<?php echo "<b>Como está estruturado o código?</b><br>"; ?>
-<i>< ?php<br>
-$num1 = ("2");<br>
-$num2 =("3");<br>
-$result = $num1+$num2;<br>
-	echo ("Resultado = $result");<br>
-? ></i>
 
 
 <?php
-echo ("<br>");
-echo ("<br>");
+//mostramos o que pretendemos fazer
 echo (">> Dividir 6 : 2<br>");
+// definimos as variáveis
 $num1 = ("6");
 $num2 =("2");
+// Definimos a variável da conta de dividir usando o / 
 $result = $num1/$num2;
+//Mostra o resultado
 echo ("<b>Resultado = </b> $result");
-echo ("<br>");
-echo ("<br>");
+
 ?>
-<?php echo "<b>Como está estruturado o código?</b><br>"; ?>
-<i>< ?php<br>
-$num1 = ("6");<br>
-$num2 =("2");<br>
-$result = $num1/$num2;<br>
-echo ("Resultado =  $result");<br>
-	? ></i>
